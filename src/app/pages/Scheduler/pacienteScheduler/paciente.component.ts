5652ceec8408228127b435feb2054dfa3644fe3b
IgnaRamos <ignacio1773@gmail.com>
2025-07-18 14:52:34 -0400

    Update paciente.component.ts

--- src/app/pages/Scheduler/pacienteScheduler/paciente.component.ts
+++ src/app/pages/Scheduler/pacienteScheduler/paciente.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-paciente',
   templateUrl: './paciente.component.html',
-  styleUrls: ['./paciente.component.css']
+  styleUrls: ['./paciente.component.css'],
+  standalone: true,
+ 
 })
 export class PacienteComponent implements OnInit {
+  rutPaciente!: string;
 
-  constructor() { }
+
+  constructor(private route: ActivatedRoute, private router:Router) { }
 
   ngOnInit() {
+    this.rutPaciente = this.route.snapshot.paramMap.get('rut')!;
+
+  }
+
+  agendarCita(){
+    this.router.navigate(['/agendarCita', this.rutPaciente]);
+  }
+
+  verPendientes(){
+    this.router.navigate(['verPendientes', this.rutPaciente]);
   }
 
 }
